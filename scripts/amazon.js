9e4b560a39d4928386b1586e2c4ae26729600fd8
xanoster <your-email@example.com>
2024-10-12 01:01:44 +0530

    add to cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -72,6 +72,15 @@ cart.push({
   quantity:1
 });
 }
-console.log(cart);
+
+let cartQuantity=0;
+
+cart.forEach((item)=>{
+  cartQuantity+=item.quantity;
+});  
+
+document.querySelector(".js-cart-quantity")
+.innerHTML=cartQuantity
+
   })
 })
