50c885c6a7175022bdf1f76bf6303b2ea4ccc291
xanoster <your-email@example.com>
2024-10-22 03:57:44 +0530

    use inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -26,27 +26,25 @@ class Product{
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`;
   }
+extraInfoHTML(){
+  return '';
+}
+}
+
+class Clothing extends Product {
+sizeChartLink;
+
+constructor(productDetails){
+ super(productDetails);
+ this.sizeChartLink=productDetails.sizeChartLink;
 
 }
 
-const product1=new Product(
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  }
-);
-
+extraInfoHTML(){
+  return `
+  <a href="${this.sizeChartLink}" target="_blank">Size Chart</a>`
+}
+}
 
 export const products = [
   {
@@ -708,5 +706,8 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+  if(productDetails.type==='clothing'){
+    return new Clothing(productDetails);
+  }
 return new Product(productDetails);
 });
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -40,6 +40,8 @@ let productsHTML='';
             </select>
           </div>
 
+          ${product.extraInfoHTML()}
+
           <div class="product-spacer"></div>
 
           <div class="added-to-cart">
