d181df543e52eaa083046e9a56847a9d29b11b55
Xanoster <techypankaj@gmail.com>
2024-11-03 10:09:20 +0100

    feat(api): implement login/signup routes

---
+++ server/routes/auth.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User.js');
+
+router.post('/signup', async (req, res) => {
+    // Signup logic
+});
+
+router.post('/login', async (req, res) => {
+    // Login logic
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.status(200).send('Hello World'));
+const authRouter = require('./routes/auth');
+app.use('/api/auth', authRouter);
 
+app.get('/', (req, res) => res.status(200).send('Amazon API Running'));
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
