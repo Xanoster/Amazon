08e60d777bb5f6e575273b6e227cd3a292763050
Xanoster <techypankaj@gmail.com>
2024-11-20 19:30:11 +0100

    chore: cleanup console logs

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './Header';
+import Home from './Home';
+import Checkout from './Checkout';
+import Login from './Login';
+import Payment from './Payment';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Amazon Clone</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/checkout" element={<><Header /><Checkout /></>} />
+          <Route path="/payment" element={<><Header /><Payment /></>} />
+          <Route path="/" element={<><Header /><Home /></>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 export default App;
